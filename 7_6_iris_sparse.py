64a7688256b507d9940d87a6cc7e73d78e3d4c3f
momingyu <river98@hanmail.net>
2024-01-24 15:57:28 +0900

    sparse

--- 7_6_iris_sparse.py
+++ 7_6_iris_sparse.py
@@ -3,11 +3,38 @@
 # iris 파일에 대해서 sparse 버전의 모델을 구축하세요
 import pandas as pd
 import tensorflow.keras as keras
+import numpy as np
+from sklearn import preprocessing
 
 def read_iris():
     iris = pd.read_csv('data/iris_onhot.csv')
-    print(iris)
+    x = iris.values[:, :-1]
+    variety = iris.values[:, -1]            # 1차원
 
+    enc = preprocessing.LabelEncoder()      # enc : encoder
+    y = enc.fit_transform(variety)
+    print(y[5])
+    exit()
 
+    return x, y
+
+x, y = read_iris()
+print(x.shape, y.shape)
+
+model = keras.Sequential()
+model.add(keras.layers.Dense(3, activation='softmax'))
+
+model.compile(optimizer=keras.optimizers.SGD(0.01),
+              loss=keras.losses.sparse_categorical_crossentropy,            # sparse_categorical_crossentropy 로 변경
+              metrics='acc')
+
+model.fit(x_train, y_train, epochs=50, verbose=2,
+          validation_data=(x_texst, y_texst))
+
+p = model.predict(x, verbose=0)
+print(p)
+
+p_arg = np.argmax(p, axis=1)
+print(p_arg)
 
 read_iris()
